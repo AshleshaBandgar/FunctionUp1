a31b5ac86fd22ac6fd245f0c0a8afd83c7d5f5c5
AshleshaBandgar <ashlesha.bandgar10@gmail.com>
2023-05-03 00:47:48 +0530

    user name

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './App.css';
-import { useState, useEffect } from 'react';
+import APICards from './components/organisms/APICards'
 
 
 export default function App() {
-  const [apiData, setApiData] = useState('');
   
-useEffect(() => {
-  fetch("https://dog.ceo/api/breeds/image/random")
-    .then( (res) => res.json() )
-    .then( (data)=> setApiData(data) ) 
-  }, [])
 
   return (
-  <div>
-    <img src={apiData.message} />
+  <div className="App">
+   <APICards/>
+   <APICards/>
+   <APICards/>
+   <APICards/>
+   <APICards/>
+  <APICards/>
   </div>
   );
 }
