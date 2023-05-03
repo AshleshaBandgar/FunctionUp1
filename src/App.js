a2c68ce1f694b660e4ecf0df405a59ad7f6e9ea1
AshleshaBandgar <ashlesha.bandgar10@gmail.com>
2023-05-03 16:32:45 +0530

    Sent props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './App.css';
-import APICards from './components/organisms/APICards'
-
+import APICards from './components/organisms/APICards';
+import {useState} from 'react'
 
 export default function App() {
-  
 
+  const [user, setUser] = useState([])
+
+  async function handleAPI(){
+      const response = await fetch('https://reqres.in/api/users/')
+      const userData = await response.json()
+      setUser(userData.data)
+      console.log(userData.data)
+  }
+  
   return (
-  <div className="App">
-   <APICards/>
-   <APICards/>
-   <APICards/>
-   <APICards/>
-   <APICards/>
-  <APICards/>
+  
+       <div>
+       <button onClick={handleAPI}>Get User</button>
+  
+       <div className="App">
+  {
+    user.map((userData) => <APICards userData={userData} key ={userData.id}/>)
+  }
+  
+  </div>
   </div>
   );
 }
--- src/components/organisms/APICards.js
+++ src/components/organisms/APICards.js
@@ -1,40 +1,23 @@
 import React from 'react';
-import './APICards.css'
-import {useEffect, useState} from 'react'
+import './APICards.css';
 
-export default function APICards(){ 
-    const [user, setUser] = useState([])
-    async function getUsers(){
-        fetch('https://reqres.in/api/users/')
-        .then((data)=> data.json())
-        .then ((response)=> setUser(response.data))
-        .catch((error) => console.log(error))  
-    
-    }
-    useEffect(() =>{
-     getUsers();
-} ,[]);
-
-  return (
-<>
-    <div className="card-container">
-        <div className="card">
-            
-                <div className="card-body">
-                <span className="card-number">{user.id}</span><br/>
-                <span className="card-author">{user.email}</span>
-                <span className="card-title">{user.first_name}</span><br/>
-                <span className="card-description">{user.last_name}</span>
-                </div>
-                  <img src={user.avatar} alt="" className="card-media"/><br/>
-                  <button>Get User</button>
-                 
-
-           
-        </div>
-       
-    </div>
-   
-</>    
+export default function APICards({userData}){ 
+  const {
+    id,
+    email,
+    first_name,
+    last_name,
+    avatar} = userData
+  
+return (
+<div className="wrapper">
+   <img src = {avatar}
+    height = "100px"
+    style = {{borderRadius : "50%"}} alt = ""/>
+    <p>{id}</p> 
+    <p>{email}</p>
+    <p>{first_name}</p>
+    <p>{last_name}</p>
+</div>    
   )
 }
