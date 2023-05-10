4fdcbad9d9c60f4648fba9e1cf0086ff961be0f7
AshleshaBandgar <ashlesha.bandgar10@gmail.com>
2023-05-10 22:51:44 +0530

    apllied css

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
-import React from 'react'
-import axios from 'axios'
-import {useState} from 'react'
 
-export default function App(){
+import styles from './App.module.css'
+import Scrollbar from './components/scrollbar/scrollbar';
 
-  const [number, setNumber] = useState(' ')
-  const [otp, setOtp] = useState(' ')
+function App() {
 
-   function valid(){
-
-    const validiateMyNumber = /^[6-9]\d{99}$/.test(number)
-    if(!validiateMyNumber){
-      setOtp(" please check your number")
-    }else{
-      setOtp(" message sent succesfully")
-      setNumber(" ")
-    }}
-  function getOtp(){
-   axios.post('https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP',{
-     mobile : `${number}`
-   })
-  
-     .then((data)=>setNumber(data.number)) 
-     .catch(error=>console.log(error)) 
-     valid()
-  }
-  
-return (
-    <div>
-    
-    <input placeholder = "Enter mobile number"
-           type = "number"
-           onChange={(e)=>{setNumber(e.target.value)}}
-           value={number}
-
-    />
-    <br/>
-    <button onClick={getOtp}>Get OTP</button>
-      
+return(
+    <div className={styles.App}>
+        <Scrollbar/>
     </div>
-  )
+  );
 }
 
+export default App;
