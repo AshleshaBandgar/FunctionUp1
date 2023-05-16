ffd7cfb277c55025889cd00af0b9183676e8c5c9
AshleshaBandgar <ashlesha.bandgar10@gmail.com>
2023-05-16 21:01:29 +0530

    Given Path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import './App.css'
-import ToDoList from './components/organisms/ToDoList'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function App(){
-  return (
-  <div>
-    
-    <ToDoList/>
 
+const navigate = useNavigate()
+
+function handleLogin(){
+  navigate('/Login')
+}
+
+function handleRegister(){
+  navigate('/Register')
+}
+
+function handleHome(){
+  navigate('/Home')
+}
+
+function handleAboutUs(){
+  navigate('/aboutus')
+}
+
+
+
+
+
+  return (
+  <div className="App">
+
+    
+       <button onClick={handleHome}>Home</button>
+       <button onClick={handleLogin}>Login</button>
+       <button onClick={handleRegister}>Register</button>
+       <button onClick={handleAboutUs}>AboutUs</button>
+      
+       
   </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Home from './components/organisms/Home';
+import Login from './components/organisms/Login';
+import Register from './components/organisms/Register';
+import AboutUs from './components/organisms/AboutUs';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div><App/></div>,
+  },
+  {
+    path: "/login",
+    element: <div><Login/></div>,
+  },
+  {
+    path: "/register",
+    element: <div><Register/></div>,
+  },
+  {
+    path: "/home",
+    element: <div><Home/></div>,
+  },
+  {
+    path: "/aboutus",
+    element: <div><AboutUs/></div>,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
