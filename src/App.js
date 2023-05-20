9a714581a71ba1bdc6979ba25edae7cfa3c67f15
AshleshaBandgar <ashlesha.bandgar10@gmail.com>
2023-05-20 22:16:35 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './App.css'
-import ToDoList from './components/organisms/ToDoList'
+import AddNewUser from './components/organisms/AddNewUser'
 
 export default function App(){
  
 return (
   
    <div className="app">
-    <ToDoList/>
+    <AddNewUser/>
    </div>
 )
 }
---
+++ src/components/organisms/AddNewUser.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+export default function AddNewUser(){
+    const[activity, setActivity] = useState("")
+    const[listData, setlistData] = useState([])
+
+    function handleActivity(){
+        //setlistData([...listData, activity]) or below method 
+        setlistData((listData)=>{
+        const updateList = [...listData, activity]   //list->to add activities below input bar
+                                                     //activities values will be listed in [...listData
+           setActivity('');                      //setActivity('')->to clear input bar
+           return updateList;
+        })
+    }
+    function handleRemove(i){
+        const updatedListData = listData.filter((elem,id)=>{   //filter method to filter out list from listData
+           return i!=id;          //to delete specific item which we want to delete
+                                  //because we assigning only one delete button
+            })
+            setlistData(updatedListData)
+        }
+    function removeAll(){
+        setlistData([]);           //delete all liistData from list
+    }
+  return (
+    <>
+        <div className="container">
+            <div className="header">Add New User</div>
+            <input type="text" placeholder='Add todolist'  //value={activity}->to store input value in variable(activity)
+                                                           //onChange->to set the input value in activity variable(otherwise u won't be able to type in input)
+                                                           //(e.target.value)->value that user is typing
+                                                           //setActivity(e.target.value)->input value will be stored in activity by using setActivity
+                   value={activity} onChange={(e)=>setActivity(e.target.value)}/>   
+                   <button onClick={handleActivity}>Add</button>
+                   <h3>Here is the names of users: {")"}</h3>
+            
+            {listData.map((data,i)=>{
+                return(
+                    <p className="List-heading" key={i}>
+                     <div>{data}</div>
+                     <div className="btn-position" onClick={()=>handleRemove(i)}>Delete</div>    
+                             {/* passing key=i in handleRemove                                                                          */}
+                     </p>
+                )
+            })
+            }
+            {listData.length>=1 && <button onClick={removeAll}>Remove all</button>}
+            
+        </div>
+    </>
+  )
+}
