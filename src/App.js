c4e111fc8a60ce8aa69b12ee19936553de922c38
AshleshaBandgar <ashlesha.bandgar10@gmail.com>
2023-05-25 15:37:29 +0530

    Changes Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from "react";
-import "./App.css";
-import SearchBar from "./components/organisms/SearchBar";
-import BookData from "./Data.json";
+import { Container, Heading } from '@chakra-ui/react';
+import { AddTodo } from './components/AddTodo';
+import { TodoList } from './components/TodoList';
+import { VisibilityFilter } from './components/VisibilityFilter';
 
 function App() {
   return (
-    <div className="app">
-      <SearchBar placeholder="Enter a Book Name..." data={BookData} />
-    </div>
+    <Container maxW="container.sm">
+      <Heading my="4">Todo List</Heading>
+      <AddTodo />
+      <TodoList />
+      <VisibilityFilter />
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,48 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import Home from './components/organisms/Home';
-import Login from './components/organisms/Login';
-import Register from './components/organisms/Register';
-import AboutUs from './components/organisms/AboutUs';
-import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div><App/></div>,
-  },
-  {
-    path: "/login",
-    element: <div><Login/></div>,
-  },
-  {
-    path: "/register",
-    element: <div><Register/></div>,
-  },
-  {
-    path: "/home",
-    element: <div><Home/></div>,
-  },
-  {
-    path: "/aboutus",
-    element: <div><AboutUs/></div>,
-  }
-]);
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>
+// Chakra UI
+import { ChakraProvider } from '@chakra-ui/react';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ChakraProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChakraProvider>
+  </Provider>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
